5d23cb975f20e124dd833dd34d04096296f34b09
Noah Lambe <noahalambe@gmail.com>
2025-05-06 13:00:56 -0230

    added dummy code to test repo

---
+++ src/Assignment1/test.java
@@ -0,0 +1,10 @@
+package Assignment1;
+
+public class test {
+    public int num = 5;
+
+    public static void main(String[] args) {
+        test t = new test();
+        System.out.println(t.num);
+    }
+}
